60540a1f03069b674fba0ec21d8011645166ec64
immicks <immicks@gmail.com>
2023-09-05 21:36:58 +0100

    finished beta version

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,113 @@
 <template>
-  <div>Hello world</div>
+  <div
+    class="min-w-screen h-screen overflow-hidden bg-lightGrey text-white sm:flex sm:flex-col sm:justify-center sm:items-start"
+  >
+    <AlertModal :email="email" @close="closeModal" v-if="success.state" />
+    <div
+      v-else
+      class="sm:w-7/12 sm:mx-auto sm:h-[70vh] sm:px-6 sm:py-4 bg-white text-darkGrey sm:rounded-3xl sm:flex sm:flex-row flex-col sm:shadow-md animate__animated animate__zoomIn animate__faster"
+    >
+      <div class="w-7/12 sm:pt-16 sm:pl-10 sm:pr-16">
+        <!-- Sign-up form start -->
+        <h1 class="text-5xl sm:font-bold">Stay updated!</h1>
+
+        <p class="py-4 text-sm font-medium">
+          Join 60,000+ product managers receiving monthly <br />
+          updates on:
+        </p>
+
+        <ul class="mt-2">
+          <li class="flex items-center space-x-4 mb-2 text-sm font-semibold">
+            <img src="images/icon-list.svg" class="h-4" alt="List" />
+            <span>Product discovery and building what matters</span>
+          </li>
+          <li class="flex items-center space-x-4 mb-2 text-sm font-semibold">
+            <img src="images/icon-list.svg" class="h-4" alt="List" />
+            <span>Measuring to ensure updates are a success</span>
+          </li>
+          <li class="flex items-center space-x-4 mb-2 text-sm font-semibold">
+            <img src="images/icon-list.svg" class="h-4" alt="List" />
+            <span>And much more!</span>
+          </li>
+        </ul>
+
+        <form action="#" method="post" class="my-8">
+          <div class="flex flex-col">
+            <div class="flex items-center justify-between">
+              <label for="email" class="text-xs font-bold">Email address</label>
+              <label
+                v-show="error.state"
+                for="email"
+                class="text-xs font-bold text-red-400"
+                >Valid email required</label
+              >
+            </div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              v-model="email"
+              @focus="() => (error.state ? (error.state = false) : '')"
+              placeholder="email@company.com"
+              class="transition outline-none border rounded-lg px-6 py-3 text-sm mt-2"
+              :class="{
+                'border-red-400 bg-red-200 text-red-600 placeholder:text-red-600':
+                  error.state,
+                'hover:border-darkGrey': !error.state,
+              }"
+            />
+          </div>
+
+          <button
+            @click.prevent="submit"
+            class="transition outline-none bg-darkGrey text-white w-full py-3 text-sm font-bold rounded-lg mt-6 duration-300 ease-linear bg-gradient-to-r hover:from-pink-500 hover:to-orange-500 hover:from-5% hover:to-95% cursor-pointer btn"
+          >
+            Subscribe to monthly newsletter
+          </button>
+        </form>
+
+        <!-- Sign-up form end -->
+      </div>
+      <div class="w-5/12">
+        <img
+          src="images/desktop-img.svg"
+          alt="Frontend mentor"
+          class="object-contain object-center h-full w-full"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import AlertModal from "./components/AlertModal.vue";
+
+const error = ref({
+  state: false,
+  msg: "",
+});
+const success = ref({
+  state: false,
+  msg: "",
+});
+const email = ref("");
+let emailReg =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+function submit() {
+  if (email.value === "" || !email.value.match(emailReg)) {
+    error.value.state = true;
+  } else {
+    success.value.state = true;
+  }
+}
+function closeModal() {
+  success.value.state = false;
+  email.value = "";
+  error.value.state = false;
+}
+</script>
 
 <style lang="scss">
 .attribution {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        primary: "hsl(4, 100%, 67%)",
+        darkGrey: "hsl(234, 29%, 20%)",
+        lightGrey: "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
